a9d5f55d89a2711f3d872dc96860869bd33875ed
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-20 17:33:02 -0500

    create user model

---
+++ server/models/user.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize.js";
+import "bcrypt";
+
+const User = sequelize.define("users", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+  },
+  instanceMethods: {
+    generateHash(password) {
+      return bcrypt.hash(password, bcrypt.genSaltSync(8));
+    },
+    validPassword(password) {
+      return bcrypt.compare(password, this.password);
+    },
+  },
+});
+
+export default User;
