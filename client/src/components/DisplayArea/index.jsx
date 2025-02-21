4230c3b30642cac6599358f75182ad21ce24a50e
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-21 00:25:02 -0500

    create display area component

---
+++ client/src/components/DisplayArea/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const DisplayArea = ({ dataset }) => {
+  const [displayedMovies, setDisplayedMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const moviesPerPage = 15;
+  const pages = Math.floor(dataset.length / moviesPerPage);
+
+  useEffect(() => {
+    setDisplayedMovies(dataset.slice(0, moviesPerPage));
+  }, []);
+
+  const handleChange = (event) => {
+    if (displayedMovies.length >= dataset.length) {
+      return;
+    }
+
+    setDisplayedMovies((prevMovies) => [
+      ...prevMovies,
+      ...dataset.slice(prevMovies.length, prevMovies.length + moviesPerPage),
+    ]);
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <Box>
+      <InfiniteScroll
+        dataLength={displayedMovies.length}
+        next={handleChange}
+        hasMore={page < pages}
+        loader={<h4>Loading</h4>}
+        endMessage={<p>That's all folks! No more movies to see!</p>}
+      >
+        <Box>
+          {displayedMovies.map((movie) => {
+            <div key={movie.id}>
+              <h3>{movie.title}</h3>
+            </div>;
+          })}
+        </Box>
+      </InfiniteScroll>
+    </Box>
+  );
+};
+
+export default DisplayArea;
