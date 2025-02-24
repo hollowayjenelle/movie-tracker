a567ba472a79a045dae2b56a0f98a4b26a16eebc
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-23 22:58:27 -0500

    create rating dialog

--- client/src/components/MovieCard/index.jsx
+++ client/src/components/MovieCard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -7,8 +7,22 @@ import {
   Typography,
   Button,
 } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import RatingDialog from "../RatingDialog";
 
 const MovieCard = ({ title, imageURL, releaseDate, voteAvg, voteCount }) => {
+  const watchedMovies = useSelector((state) => state.watchedMovies.movies);
+  const [rating, setRating] = useState(2);
+  const [openDialog, setOpenDialog] = useState(false);
+  const hasWatched = watchedMovies.find((movie) => movie.title === title);
+
+  const handleOpenDialog = () => setOpenDialog(true);
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+  const handleAddWatched = () => {
+    console.log("Time to add to watched list");
+  };
   return (
     <Card sx={{ maxWidth: 300 }}>
       <CardMedia
@@ -24,8 +38,16 @@ const MovieCard = ({ title, imageURL, releaseDate, voteAvg, voteCount }) => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button>Add to Watch List</Button>
+        <Button onClick={handleOpenDialog}>Add to Watch List</Button>
       </CardActions>
+      <RatingDialog
+        onSetRating={setRating}
+        rating={rating}
+        open={openDialog}
+        onClose={handleCloseDialog}
+        movieTitle={title}
+        onSubmit={handleAddWatched}
+      />
     </Card>
   );
 };
---
+++ client/src/components/RatingDialog/index.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  Rating,
+  Box,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+
+const RatingDialog = ({
+  onSetRating,
+  rating,
+  open,
+  onClose,
+  movieTitle,
+  onSubmit,
+}) => {
+  const [hover, setHover] = useState(-1);
+  const ratings = {
+    0.5: "Worst movie ever!",
+    1: "Hate it",
+    1.5: "Horrible",
+    2: "Meh",
+    2.5: "Not too bad",
+    3: "It was okay",
+    3.5: "It was good",
+    4: "It was great",
+    4.5: "Excellent movie",
+    5: "Best movie ever!",
+  };
+
+  const getLabelText = (value) => {
+    return `${value} Star${value !== 1 ? "s" : ""}, ${ratings[value]}`;
+  };
+
+  return (
+    <Dialog
+      onClose={onClose}
+      open={open}
+      sx={{
+        ".MuiDialog-paper": { height: 150, width: 400, alignItems: "center" },
+      }}
+    >
+      <DialogTitle>How would you rate {movieTitle}?</DialogTitle>
+      <Box
+        sx={{
+          width: 280,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Rating
+          name="movie-rating"
+          value={rating}
+          precision={0.5}
+          getLabelText={(value) => getLabelText(value)}
+          onChange={(event, value) => {
+            onSetRating(value);
+          }}
+          onChangeActive={(event, value) => {
+            setHover(value);
+          }}
+          emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+        />
+        {rating !== null && (
+          <Box sx={{ ml: 2 }}>{ratings[hover !== -1 ? hover : rating]}</Box>
+        )}
+      </Box>
+      <DialogActions>
+        <Button onClick={onSubmit}>Submit Rating</Button>
+        <Button onClick={onClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default RatingDialog;
