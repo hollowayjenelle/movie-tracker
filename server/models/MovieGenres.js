8d066b4e2ff3d96966fb718dcec95cf5d43523e8
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-26 00:50:07 -0500

    create MovieGenres relationship model

---
+++ server/models/MovieGenres.js
@@ -0,0 +1,23 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/sequelize";
+
+const MovieGenres = sequelize.define("MovieGenres", {
+  movieId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "movies",
+      key: "id",
+    },
+    onDelete: "CASCADE",
+  },
+  genreId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: "genres",
+      key: "id",
+    },
+    onDelete: "CASCADE",
+  },
+});
+
+export default MovieGenres;
--- server/relationships/index.js
+++ server/relationships/index.js
@@ -2,10 +2,11 @@ import Genre from "../models/genre.js";
 import Movie from "../models/movie.js";
 import Actor from "../models/actor.js";
 import MovieActors from "../models/MovieActors.js";
+import MovieGenres from "../models/MovieGenres.js";
 
 const associate = () => {
-  Movie.belongsToMany(Genre, { through: "MovieGenres" });
-  Genre.belongsToMany(Movie, { through: "MovieGenres" });
+  Movie.belongsToMany(Genre, { through: MovieGenres });
+  Genre.belongsToMany(Movie, { through: MovieGenres });
   Movie.belongsToMany(Actor, { through: MovieActors });
   Actor.belongsToMany(Movie, { through: MovieActors });
 };
