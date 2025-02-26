000482c7dcbcf10d06cd429f8bd6917f21a154f3
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-26 00:31:27 -0500

    disable ssl

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import app from "./app.js";
 import { connectDB } from "./service/connectDB.js";
 import "dotenv/config";
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const startServer = () => {
   connectDB();
   app.listen(process.env.PORT || 3000, () => {
