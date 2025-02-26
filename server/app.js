0957424e3eb2682e894d1ee1b3219c529c610fdc
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-26 01:16:51 -0500

    update app.js

--- server/app.js
+++ server/app.js
@@ -4,22 +4,9 @@ import genresRouter from "./router/genres.js";
 import express from "express";
 import cors from "cors";
 
-import { createRequire } from "module";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const require = createRequire(import.meta.url);
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
-});
 
 app.use("/api/movies", moviesRouter);
 app.use("/api/actors", actorsRouter);
