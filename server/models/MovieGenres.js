911606f0198da990eecf09b6ab88cd0a3a3f6650
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-26 00:51:30 -0500

    update MovieGenres relationship model

--- server/models/MovieGenres.js
+++ server/models/MovieGenres.js
@@ -1,5 +1,5 @@
 import { DataTypes } from "sequelize";
-import sequelize from "../config/sequelize";
+import sequelize from "../config/sequelize.js";
 
 const MovieGenres = sequelize.define("MovieGenres", {
   movieId: {
