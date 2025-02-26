257746952d34d4e4a1ee1a4d2d972bbda386b77f
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-25 19:06:58 -0500

    set up vercel config

--- server/app.js
+++ server/app.js
@@ -4,9 +4,16 @@ import genresRouter from "./router/genres.js";
 import express from "express";
 import cors from "cors";
 
+const path = require("path");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 app.use("/api/movies", moviesRouter);
 app.use("/api/actors", actorsRouter);
