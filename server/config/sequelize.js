db1f550ed13bf6b0636d9dae9b468a5ea599b144
Jenelle Holloway <hollowayjenelle@gmail.com>
2025-02-26 00:01:51 -0500

    Update sequelize initialization file

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -10,17 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "\\config.json")[env];
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "postgres",
-//   }
-// );
+const config = require(__dirname + "/config.json")[env];
 
 const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
